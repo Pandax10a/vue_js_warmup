9f30b4db5957bc898157295c753c4eb5c7e10193
panda1 <blueblue0011@gmail.com>
2022-08-11 15:30:32 -0700

    stuck on 5

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <fun-component/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FunComponent from './components/FunComponent.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { FunComponent },
+  
+       
+          
+        
+    }
+
+
+
 </script>
 
 <style>
---
+++ src/components/FunComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <button id = "button1">Button</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'fun-component'
+    }
+        
+    
+    document.getElementById(`button1`).addEventListener(`click`, turn_blue)
+    function turn_blue () {
+        document.body.insertAdjacentHTML(`beforeend`, '<h1>testing to see if works</h1>');
+        document.body[`style`][`background-color`]= 'blue';
+    }
+    
+</script>
+
+<style scoped>
+
+</style>
