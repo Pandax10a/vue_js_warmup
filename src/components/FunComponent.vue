a696935349af3f2bdfcd5d5f5013dcd9b2f83c50
panda1 <blueblue0011@gmail.com>
2022-08-26 19:09:34 -0700

    9 is done but 9.1, cant get the individual tag to change but rn changes all the p tag at once

--- src/components/FunComponent.vue
+++ src/components/FunComponent.vue
@@ -1,23 +1,53 @@
 <template>
     <div>
         <button id = "button1">Button</button>
+        <section v-for="(secret, index) in secrets" :key="index"  :class="{pinkPink}" @click = "pinkPink = !pinkPink">
+           
+            <p >{{secret}}</p>
+        
+        </section>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'fun-component'
-    }
+        data() {
+            return {
+                secrets: [`aa`, `bb`, `cc`, `dd`],
+                // alternative method of changing color by adding css component to data ( :style "myStyle" )add to element
+                myStyle:{
+                background: "pink"
+            },
+            pinkPink: false
+            }
+        },
+        mounted () {
+            document.getElementById(`button1`).addEventListener(`click`, this.turn_blue);
+        },
+        name: 'fun-component',
+
+        methods: {
+            turn_blue() {
+                      document.body.insertAdjacentHTML(`beforeend`, '<h1>testing to see if works</h1>');
+        document.body[`style`][`background`]= 'blue';
+            },
+          
+        }
+       
+        }
+        
+
+    
         
     
-    document.getElementById(`button1`).addEventListener(`click`, turn_blue)
-    function turn_blue () {
-        document.body.insertAdjacentHTML(`beforeend`, '<h1>testing to see if works</h1>');
-        document.body[`style`][`background-color`]= 'blue';
-    }
+   
+ 
     
 </script>
 
 <style scoped>
-
+.pinkPink {
+    
+    background: pink;
+}
 </style>
